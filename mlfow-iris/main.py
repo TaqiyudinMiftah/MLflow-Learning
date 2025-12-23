43f75db6d6054943ea3fb7e99ea714ca0b19bc4e
Taqiyudin <taqiyudinmiftah.01@gmail.com>
2025-12-24 00:16:17 +0700

    main

--- mlfow-iris/main.py
+++ mlfow-iris/main.py
@@ -4,39 +4,40 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import pandas as pd
+
+# SET EXPERIMENT (AMAN DI SEMUA VERSI)
+mlflow.set_experiment("iris-classification-rf")
 
 def main():
-    # Load dataset
     X, y = load_iris(return_X_y=True)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    with mlflow.start_run():
-        # Model
+    with mlflow.start_run(run_name="rf-baseline-v1"):    
+        mlflow.set_tag(
+            "experiment_description",
+            "Eksperimen klasifikasi Iris menggunakan RandomForest sebagai baseline model"
+        )
+
         model = RandomForestClassifier(
             n_estimators=100,
             random_state=42
         )
         model.fit(X_train, y_train)
 
-        # Evaluation
         y_pred = model.predict(X_test)
         acc = accuracy_score(y_test, y_pred)
 
-        # Log metric
         mlflow.log_metric("accuracy", acc)
 
-        # Log model + input example
         mlflow.sklearn.log_model(
-            sk_model=model,
+            model,
             artifact_path="model",
             input_example=X_test[:5]
         )
 
-        print("Training selesai")
         print("Accuracy:", acc)
 
 if __name__ == "__main__":
